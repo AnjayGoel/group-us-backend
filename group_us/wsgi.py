0331fa6dff78d5db23adad803bc47046f5a7b5ca
Anjay Goel <angoel123@gmail.com>
2021-06-28 01:21:16 +0530

    New Server

--- group_us/wsgi.py
+++ group_us/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+from .secrets import *
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'group_us.settings')
 application = get_wsgi_application()
